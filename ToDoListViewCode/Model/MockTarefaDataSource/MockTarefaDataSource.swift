ed0a3ca5013f5a0384b5f8943e9ae23dcaf4e587
renato <felicio95.jr@gmail.com>
2022-03-26 11:38:29 -0300

    adds new mock data

---
+++ ToDoListViewCode/Model/MockTarefaDataSource/MockTarefaDataSource.swift
@@ -0,0 +1,28 @@
+//
+//  MockTarefaDataSource.swift
+//  ToDoListViewCode
+//
+//  Created by Renato F. dos Santos Jr on 26/03/22.
+//
+
+import Foundation
+
+
+struct TarefaDataSource {
+    static var tarefas: [TarefaData] = [.init(id: UUID(), title: "Finalizar ToDoApp", detail: "Consumir CoreData", isDone: false),
+                                        .init(id: UUID(), title: "Finalizar ToDoApp", detail: "Consumir SecondViewController", isDone: false),
+                                        .init(id: UUID(), title: "Finalizar ToDoApp", detail: "Consumir CoreData", isDone: false),
+                                        .init(id: UUID(), title: "Finalizar ToDoApp", detail: "Consumir SecondViewController", isDone: false),
+                                        .init(id: UUID(), title: "Finalizar ToDoApp", detail: "Consumir CoreData", isDone: false),
+                                        .init(id: UUID(), title: "Finalizar ToDoApp", detail: "Consumir SecondViewController", isDone: false),
+                                        .init(id: UUID(), title: "Finalizar ToDoApp", detail: "Consumir CoreData", isDone: false),
+                                        .init(id: UUID(), title: "Finalizar ToDoApp", detail: "Consumir SecondViewController", isDone: false),
+                                        .init(id: UUID(), title: "Finalizar ToDoApp", detail: "Consumir CoreData", isDone: false),
+                                        .init(id: UUID(), title: "Finalizar ToDoApp", detail: "Consumir SecondViewController", isDone: false),
+                                        .init(id: UUID(), title: "Finalizar ToDoApp", detail: "Consumir CoreData", isDone: false),
+                                        .init(id: UUID(), title: "Finalizar ToDoApp", detail: "Consumir SecondViewController", isDone: false),
+                                        .init(id: UUID(), title: "Finalizar ToDoApp", detail: "Consumir CoreData", isDone: false),
+                                        .init(id: UUID(), title: "Finalizar ToDoApp", detail: "Consumir SecondViewController", isDone: false),
+                                        .init(id: UUID(), title: "Finalizar ToDoApp", detail: "Consumir CoreData", isDone: false),
+                                        .init(id: UUID(), title: "Finalizar ToDoApp", detail: "Consumir SecondViewController", isDone: false)]
+}
