e306e0df48d16eb428827b762bf52530e2c513f9
renato <felicio95.jr@gmail.com>
2022-03-26 10:21:09 -0300

    Initial project configs

--- ToDoListViewCode/Scenes/Home/HomeViewController.swift
+++ ToDoListViewCode/Scenes/Home/HomeViewController.swift
@@ -7,11 +7,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        title = "Tarefas"
+        
+        view.backgroundColor = .lightGray
     }
 
 
