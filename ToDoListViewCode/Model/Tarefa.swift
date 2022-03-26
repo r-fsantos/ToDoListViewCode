b70cce1b01724bff355fe6c5abcd44bc59d86082
renato <felicio95.jr@gmail.com>
2022-03-26 10:26:49 -0300

    Adds model and service layer

---
+++ ToDoListViewCode/Model/Tarefa.swift
@@ -0,0 +1,8 @@
+//
+//  Tarefa.swift
+//  ToDoListViewCode
+//
+//  Created by Renato F. dos Santos Jr on 26/03/22.
+//
+
+import Foundation
---
+++ ToDoListViewCode/Service/Service.swift
@@ -0,0 +1,8 @@
+//
+//  Service.swift
+//  ToDoListViewCode
+//
+//  Created by Renato F. dos Santos Jr on 26/03/22.
+//
+
+import Foundation
